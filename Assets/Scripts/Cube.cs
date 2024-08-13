4567ed156233229f71839a929bab27acc8cb8023
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-13 12:57:32 +0200

    worked on the readme and added documentation to the code

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,20 +2,25 @@
 
 public class Cube : MonoBehaviour
 {
+    // Declaring the variable.
     public MeshRenderer Renderer;
     
     private void Start()
     {
+        // Sets the psoition and scale of the cube.
         transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
         
+        // Saving the material of the MeshRenderer (3D object renderer) so it can later be manipulated.
         Material material = Renderer.material;
         
+        // Sets the Color for the Cube based on RGB values.
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
     
     private void Update()
     {
+        // Rotates the cube around the xAngle.
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
     }
 }
--- README.md
+++ README.md
@@ -3,5 +3,17 @@
 This repository contains the first checkpoint from the Unity Pathway "Junior Programmer". After passing the initial quiz the instructions were as follows.
 
 ## Instructions
-1. TBD
+1. Import the assets
+2. Observe the Cubes behaviour 
+3. Implementing the following Changes to the Cube:
+	- Change the cube's location (transform).
+	- Change the cube's scale.
+	- Change the angle at which the cube rotates.
+	- Change the cube’s rotation speed.
+	- Change the cube’s material color.
+	- Change the cube’s material opacity.
+	- Modify any of the changes above so they change randomly each time the scene is played.
+	- Change the color of the cube over time.
+4. Create a WebGL build
+5. Upload the build. You can find the link [here](TBD)
 
